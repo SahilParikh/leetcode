2c197262091a5106bee887c80af47be84979c8d3
SahilParikh <sparikh@udel.edu>
2020-08-23 23:10:13 -0400

    Update non_decreasing_array.py

--- non_decreasing_array.py
+++ non_decreasing_array.py
@@ -3,24 +3,26 @@ def non_dec_array(array):
     Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
     We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
     '''
-    i = 0
+def non_decr_array(int_list):
     counter = 0
-    while i < len(array):
-        if i != len(array) - 1:
-            if array[i] > array[i + 1]:       
-                array[i] = array[i+1] - 1
-                counter += 1
-        else:
-            if counter >= 2:
-                return('False')
+    for i in range(len(int_list)-1):
+        if int_list[i] > int_list[i+1]:
+            counter += 1
+            if i == 0:
+                int_list[i] = int_list[i+1]
             else:
-                if array[i] > array[i-1]: 
-                    return('True')
-                else:
-                    return('False')
-        i += 1
-        
-non_dec_array([4,2,3])
-non_dec_array([4,2,1])
-non_dec_array([1,2,3])        
+                int_list[i+1] = int_list[i]            
+    if counter > 1:
+        print('False')
+    else:
+        print('True')
+
+
+non_decr_array([4,2,1])
+non_decr_array([4,2,3])
+non_decr_array([4,7,3,5,6])
+non_decr_array([4,2,3,5,6])
+non_decr_array([13,17,6,20,21])  
+non_decr_array([4,134,3,13431,6])
+    
         
